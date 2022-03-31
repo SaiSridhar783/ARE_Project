eda9a8d59f7b739514d13f53106bf1bb20e58fcd
Sai Sridhar Akula <sridhar.akula001@gmail.com>
2022-03-31 09:32:45 +0530

    Styling and print page finishing touches

--- renderer/pages/_document.jsx
+++ renderer/pages/_document.jsx
@@ -1,24 +1,34 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        ></link>
-        <link
-          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap"
-          rel="stylesheet"
-        ></link>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html style={{ margin: 0, padding: 0 }} className="main-body">
+			<Head>
+				<link
+					rel="preconnect"
+					href="https://fonts.googleapis.com"
+				></link>
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin="true"
+				></link>
+				<link
+					href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap"
+					rel="stylesheet"
+				></link>
+			</Head>
+			<body style={{ margin: 0, padding: 0 }} className="main-body">
+				<Main />
+				<NextScript />
+				<style jsx>
+					{`
+						.main-body::-webkit-scrollbar {
+							display: none;
+						}
+					`}
+				</style>
+			</body>
+		</Html>
+	);
 }
